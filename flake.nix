04238009bc39af48fe87a81e4ba77978078dab1e
Cainã Costa <me@cfcosta.com>
2024-08-12 12:48:14 -0300

    chore: initial import

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+
+    rust-dev-tools = {
+      url = "github:cfcosta/rust-dev-tools.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-utils.follows = "flake-utils";
+    };
+  };
+
+  outputs =
+    { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default = pkgs.mkShell { packages = with pkgs; [ hugo ]; };
+      }
+    );
+}
