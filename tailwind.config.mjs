906bc8649c38b0f4dd95c3c0e05865d6ffe4e9b7
Miguel Tavares <migueloliveiradev@gmail.com>
2024-09-12 17:13:14 -0300

    usage of @layer base and add background color

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,6 +2,9 @@
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
+    colors: {
+      'background': 'hsl(240, 10%, 3.9%)'
+    },
     extend: {},
   },
   plugins: [],
