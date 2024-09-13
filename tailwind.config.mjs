2c4ad2948ebc3cbe573bee08b78c578b8c5c169d
Miguel Tavares <migueloliveiradev@gmail.com>
2024-09-13 13:57:21 -0300

    add colors in tailwind config

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,7 +3,9 @@ export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     colors: {
-      'background': 'hsl(240, 10%, 3.9%)'
+      'background': 'hsl(240, 10%, 3.9%)',
+      'header': '#fafafa',
+      'paragraph': '#a1a1aa',
     },
     extend: {},
   },
